b3cee923f37b1a87f52519d469fec159caae5434
G Mohammed Thabrez <79008860+mdthabrez@users.noreply.github.com>
2021-11-02 23:51:14 +0530

    4A - Watermelon

---
+++ solutions/4A-Watermelon.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+
+ using namespace std;
+
+int main()
+{
+    int n=1;
+    cin>>n;
+    if(n>3&&n%2==0)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout<<"NO";
+    } 
+    
+}
